be09e3835b07943858880fdf7f328ed4a955f0e1
Enjl <enjlectric@gmail.com>
2022-10-02 19:02:46 +0900

    guessable adjustment

--- Assets/Scripts/GuessableImage.cs
+++ Assets/Scripts/GuessableImage.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
+[System.Flags]
+public enum ValidColors {
+    Black = 1,
+    Grey = 2,
+    Red = 4,
+    Orange = 8,
+    Yellow = 16,
+    Green = 32,
+    Blue = 64,
+    Purple = 128
+}
+
 [System.Serializable]
 public class Guessable
 {
-    [HorizontalGroup]
     [HideLabel]
     [PreviewField(Height = 32, Alignment = ObjectFieldAlignment.Left)]
     public Sprite texture;
-    [HorizontalGroup]
-    [HideLabel]
-    [Range(1, 10)]
-    public int difficulty = 1;
+    [Range(1, 4)]
+    public int strokeComplexity = 1;
+    public ValidColors colors = ValidColors.Black;
     public List<string> tags;
 }
 
