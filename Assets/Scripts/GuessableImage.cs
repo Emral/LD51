0cfea5ca0fca6cb9c0443ecb3a471d1d5b5f1299
Enjl <enjlectric@gmail.com>
2022-10-04 18:37:07 +0900

    fixes

--- Assets/Scripts/GuessableImage.cs
+++ Assets/Scripts/GuessableImage.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+#if UNITY_EDITOR
 using Sirenix.OdinInspector.Editor;
-
+#endif
 [System.Flags]
 public enum ValidColors {
     Black = 1,
