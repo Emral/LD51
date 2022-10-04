6ace2da2dace7c143dabe78b7ad61c47ecdbc7de
Enjl <enjlectric@gmail.com>
2022-10-04 17:56:06 +0900

    added sounds to elvis, fixed colors

--- Assets/Scripts/GuessableImage.cs
+++ Assets/Scripts/GuessableImage.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 
 [System.Flags]
 public enum ValidColors {
@@ -18,18 +19,60 @@ public enum ValidColors {
 [System.Serializable]
 public class Guessable
 {
+
+    public static readonly Dictionary<Color, ValidColors> colorMap = new Dictionary<Color, ValidColors>()
+    {
+        [new Color(29 / 255.0f, 31 / 255.0f, 38 / 255.0f)] = ValidColors.Black,
+        [new Color(111 / 255.0f, 109 / 255.0f, 142 / 255.0f)] = ValidColors.Grey,
+        [new Color(153 / 255.0f, 18 / 255.0f, 54 / 255.0f)] = ValidColors.Red,
+        [new Color(204 / 255.0f, 89 / 255.0f, 7 / 255.0f)] = ValidColors.Orange,
+        [new Color(206 / 255.0f, 196 / 255.0f, 82 / 255.0f)] = ValidColors.Yellow,
+        [new Color(0 / 255.0f, 195 / 255.0f, 110 / 255.0f)] = ValidColors.Green,
+        [new Color(99 / 255.0f, 129 / 255.0f, 211 / 255.0f)] = ValidColors.Blue,
+        [new Color(197 / 255.0f, 102 / 255.0f, 219 / 255.0f)] = ValidColors.Purple
+    };
+
     [HideLabel]
     [PreviewField(Height = 32, Alignment = ObjectFieldAlignment.Left)]
+    [OnValueChanged("SetColors", InvokeOnUndoRedo = true)]
     public Sprite texture;
     [Range(1, 4)]
     public int strokeComplexity = 1;
+
     public ValidColors colors = ValidColors.Black;
+
+
     public List<string> tags;
+
+    public void SetColors()
+    {
+        var arr = texture.texture.GetPixels((int)texture.rect.x, (int)texture.rect.y, (int)texture.rect.width, (int)texture.rect.height);
+        colors = (ValidColors)0;
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            if (arr[i].a > 0)
+            {
+                arr[i].r = Mathf.Floor(arr[i].r * 255) / 255.0f;
+                arr[i].g = Mathf.Floor(arr[i].g * 255) / 255.0f;
+                arr[i].b = Mathf.Floor(arr[i].b * 255) / 255.0f;
+                if (colorMap.ContainsKey(arr[i]))
+                {
+                    var c = colorMap[arr[i]];
+                    if ((colors & c) == 0)
+                    {
+                        colors = (ValidColors)((int)colors + (int)c);
+                    }
+                }
+            }
+        }
+    }
 }
 
 [CreateAssetMenu(fileName = "Guessable", menuName = "Data/Guessables")]
-public class GuessableImage : ScriptableObject
+public class GuessableImage : SerializedScriptableObject
 {
+
+    [Searchable]
     public List<Guessable> images = new List<Guessable>();
 
     private Dictionary<string, List<Guessable>> guessablesByTag = new Dictionary<string, List<Guessable>>();
@@ -57,8 +100,11 @@ public class GuessableImage : ScriptableObject
         var upperLimit = Mathf.FloorToInt((SessionVariables.Experience + 0.5f - bias * 0.5f));
         var candidates = new List<Guessable>();
 
-        while (upperLimit < 9 && candidates.Count == 0)
+        bool hasTested = false;
+
+        while (!hasTested || (upperLimit < 9 && candidates.Count == 0))
         {
+            hasTested = true;
             foreach (var image in sprites)
             {
                 if ((SessionVariables.Colors & image.colors) == image.colors)
